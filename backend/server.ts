a98281dfd6d60deecfaf665247a2541c9e532908
Yu Hamada <yuham1014@gmail.com>
2023-11-14 19:14:15 -0800

    created server.ts and connected to mongodb

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import "dotenv/config";
+import app from "./app";
+import mongoose from 'mongoose';
 
-const app: Express = express();
-const PORT: number = 5000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, Hello!");
-  });
+const port = process.env.PORT;
+const mongoUrl = process.env.MONGOURL;
+
+if (!mongoUrl) {
+  throw new Error('MongoDB URI is not defined in .env file');
+}
+
+// connecting database
+mongoose.connect(mongoUrl as string).then(() => {
+  console.log("Connected to the database");
+}).catch((err) => {
+  console.error(err);
+});
+
+
   
-  app.listen(PORT, () => {
-    console.log(`port ${PORT} でサーバー起動中`);
-  });
+app.listen(port, () => console.log(`Server running on port ${port}`));
