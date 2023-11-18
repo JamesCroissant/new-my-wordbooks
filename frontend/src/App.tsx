c61b11cd60bdbfde1025f96d34e7fabf702ccf14
Yu Hamada <yuham1014@gmail.com>
2023-11-18 11:15:50 -0800

    created Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
